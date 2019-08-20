ce297bac9285da901adffa74196ec0b4104f83a9
wookiee2187 <nlingareddy@uchicago.edu>
2019-08-20 15:04:23 -0500

    cleaner

--- test_jinja.py
+++
@@ -1,6 +0,0 @@
-from jinja2 import Environment, FileSystemLoader
-
-#env = Environment(loader = FileSystemLoader('./templates'), trim_blocks=True, lstrip_blocks=True)
-{% set request_name = "request.name"}
-template = env.get_template('deployNservice.yaml')
-temp_up = template.render(config_data)
